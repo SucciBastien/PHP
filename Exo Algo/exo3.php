4cf0782157329607a150fa56ea5ef5fc06c6dfd9
Bastien Succi <postbacsuccibastien@gmail.com>
2023-10-25 22:32:34 +0200

    exos algo while

---
+++ Exo Algo/exo3.php
@@ -0,0 +1,115 @@
+<?php
+echo "Question 1 : " . "\n";
+
+$nb = readline("Entrez un nombre : ");
+if ($nb < 0) 
+    echo "Votre nombre est négatif." . "\n";
+else 
+    echo "Votre nombre est positif." . "\n";
+
+echo "Question 2 : " . "\n";
+
+$nb1 = readline("Entrez un premier nombre : ");
+$nb2 = readline("Entrez un second nombre : ");
+if ($nb1>0 xor $nb2<0)
+    echo "Le produit de vos nombres est positif." . "\n";
+else
+    echo "Le produit de vos nombres est négatif." . "\n";
+
+echo "Question 3 : " . "\n";
+
+$nb = readline("Entrez un nombre : ");
+if ($nb < 0) 
+    echo "Votre nombre est négatif." . "\n";
+else if ($nb > 0)
+        echo "Votre nombre est positif." . "\n";
+    else 
+        echo "Votre nombre est positif et négatif." . "\n";
+
+echo "Question 4 : " . "\n";
+
+$nb1 = readline("Entrez un premier nombre : ");
+$nb2 = readline("Entrez un second nombre : ");
+if (($nb1>0 xor $nb2<0) and $nb1!=0 and $nb2!=0)
+    echo "Le produit de vos nombres est positif." . "\n";
+elseif ($nb1==0 or $nb2==0)
+        echo "Le produit de vos nombres est positif et négatif." . "\n";
+    else 
+        echo "Le produit de vos nombres est négatif." . "\n";
+
+echo "Question 5 : " . "\n";
+
+$age = readline("Entrez l'age de votre enfant : ");
+if ($age>5 and $and<8)
+    echo "Votre enfant est un Poussin." . "\n";
+else if ($age>7 and $age<10)
+        echo "Votre enfant est un Pupille." . "\n";
+    else if ($age>9 and $age<12)
+            echo "Votre enfant est un Minime." . "\n";
+        else if ($age>11)
+                echo "Votre enfant est un Cadet." . "\n";
+            else 
+                echo "Votre enfant n'est pas dans un catégorie." . "\n";
+
+echo "Question 6 : " . "\n";
+
+$heure = readline("Entrez l'Heure sans les minutes : ");
+$minute = readline("Entrez les minutes : ");
+if ($minute<59)
+    echo "Il sera " . $heure . "h" . ($minute + 1) . "\n";
+else if (($minute=59) and ($heure<23))
+        echo "Il sera " . ($heure + 1) . "h00" . "\n";
+    else
+        echo "Il sera 00h00" . "\n";
+
+echo "Question 7 : " . "\n";
+
+$heure = readline("Entrez l'Heure sans les minutes : ");
+$minute = readline("Entrez les minutes : ");
+$seconde = readline("Entrez les secondes : ");
+if ($seconde<59)
+    echo "Il sera " . $heure . "h" . $minute . " et " . ($seconde + 1) . "s" . "\n";
+else if (($seconde=59) and ($minute<59))
+        echo "Il sera " . $heure . "h" . ($minute + 1) . " et 00s" . "\n";
+    else if (($seconde=59) and ($minute=59) and ($heure<23))
+            echo "Il sera " . ($heure + 1) . "h00 et 00s" . "\n";
+        else 
+            echo "Il sera 00h00 et 00s" . "\n";
+
+echo "Question 8 : " . "\n";
+
+$nbPhotocopie = readline("Entrez le nombre de photocopies : ");
+if ($nbPhotocopie<11 and $nbPhotocopie>0)
+    echo "Le prix sera de " . ($nbPhotocopie * 0.10) . ("€") . "\n";
+else if ($nbPhotocopie<31 and $nbPhotocopie>0)
+        echo "Le prix sera de " . (1 + ($nbPhotocopie - 10) * 0.09) . ("€") . "\n";
+    else if ($nbPhotocopie>31)
+            echo "Le prix sera de " . (1.90 + ($nbPhotocopie - 30) * 0.08) . ("€") . "\n";
+        else   
+            echo "Nombre de photocopies invalide !" . "\n";
+
+echo "Question 9 : " . "\n";
+
+$sexe = readline("Entrez H si vous êtes un Homme ou F si vous êtes une Femme : ");
+$age = readline("Entrez votre age : ");
+if (($sexe=="H") and ($age>20))
+    echo "Vous êtes imposable." . "\n";
+else if ($sexe="F" and $age>17 and $age<36)
+        echo "Vous êtes imposable." . "\n";
+    else if ($sexe="H" or $sexe="F")
+        echo "Vous n'êtes pas imposable." . "\n";
+
+
+echo "Question 10 : " . "\n";
+
+$jour = readline("Entrez le jour : ");
+$mois = readline("Entrez le mois : ");
+$annee = readline("Entrez l'année' : ");
+if ($jour==29 and $mois==2)
+    echo "C'est une année bissextile." . "\n";
+else if ((fmod($annee, 4)==0) or (fmod($annee, 400)==0) and (fmod($annee, 100)!=0))
+        echo "C'est une année bissextile" . "\n";
+    else
+        echo "Ce n'est pas une année bissextile" . "\n";
+
+?>
---
+++ Exo Algo/exo4.php
@@ -0,0 +1,60 @@
+<?php
+
+// echo "Question 1 :\n";
+
+// $nb = 10;
+// while ($nb<1 or $nb>3)
+//     $nb = readline("Entrez un nombre entre 1 et 3 : ");
+
+// echo "Question 2 :\n";
+
+// $nb = 100;
+// while ($nb<10 or $nb>20){
+//     $nb = readline("Entrez un nombre entre 10 et 20 : ");
+//     if ($nb<10)
+//         echo "Plus Grand !\n";
+//     elseif ($nb>20)
+//         echo "Plus Petit !\n";
+// }    
+
+// echo "Question 3 :\n";
+
+// $nb = readline("Entrez un nombre : ");
+// $i = 0;
+
+// while ($i<10){
+//     $i++;
+//     echo ($nb + $i) . "\n";
+// }
+
+echo "Question 4 :\n";
+
+$nb1 = readline("Entrez un premier nombre : ");
+$nb2 = readline("Entrez un second nombre : ");
+$reste = 1;
+$PGCD = min($nb1, $nb2);
+
+while ($reste!=0){
+    $reste = fmod($nb1, $PGCD) + fmod($nb2, $PGCD);
+    $PGCD--;
+}
+echo "Le PGCD de ces deux chiffres est : " . ($PGCD +1) . "\n";
+
+echo "Question 5 :\n";
+
+$nb1 = readline("Entrez un premier nombre : ");
+$nb2 = readline("Entrez un second nombre : ");
+$PPCM = 1;
+$PGCD = min($nb1, $nb2);
+$reste = 1;
+
+while ($reste!=0){
+    $reste = fmod($nb1, $PGCD) + fmod($nb2, $PGCD);
+    $PGCD--;
+}
+
+$PGCD++;
+echo $PGCD . "\n";
+
+$PPCM = ($nb1 * $nb2)/$PGCD . "\n";
+echo "Le PPCM de ces deux nombre est : $PPCM.\n";
